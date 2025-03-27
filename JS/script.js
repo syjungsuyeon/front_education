312d9a70a5e7056404a33088272300cf4ff5779e
syjungsuyeon <jjj950062714@gmail.com>
2025-03-27 14:29:28 +0900

    html+css+js공부

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+function oo() {
+    //alert(1+3);
+    //alert(5*8);
+    // document.querySelector("#demo").innerHTML = 5 * 8;
+    // html요소 잡아낼 때 사용
+    // document.querySelector('p').innerText='정수연';
+    // for(let i=1;i<3000;i++){
+    //     console.log(1);  // (중요)브라우저 콘솔(개발자도구)에 쓰기
+    // }
+    // document.write("정수연");
+    console.warn(11);
+    console.dir(document.querySelector('p'));
+    console.log(document.querySelector('p'));
+    console.log(console);
+}
+function change() {
+    document.querySelector("#demo").innerHTML = "change";
+}
