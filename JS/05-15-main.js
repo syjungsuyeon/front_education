6ff8527057b7d5a0fa01b7dd8f943546dd9969ad
syjungsuyeon <jjj950062714@gmail.com>
2025-03-28 17:10:18 +0900

    0328수업실습

---
+++ JS/05-15-main.js
@@ -0,0 +1,13 @@
+// import { export이름1, export이름2 } from 'OOO.js';
+//개별 import
+import { plus, minus, height, weight, Animal, color, age } from './05-15-module.js';
+
+console.log(pplus());
+console.log(age());
+console.log(plus());
+console.log(minus());
+console.log(height);
+console.log(weight);
+const animal = new Animal('ShuShu');
+console.log(animal.name);
+console.log(color);
